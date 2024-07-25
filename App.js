cf1ecb021dfaa3e29a382b9b3c51b3b445746b4d
Ajith <ajith@dbcyelagiri.edu.in>
2024-07-25 15:13:21 +0530

    intial page design

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import { Text, View } from "react-native";
+//import liraries
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import AppNavigator from './src/navigation/Appnav';
 
-function App() {
+// create a component
+const App = () => {
   return (
-    <View>
-        <Text>Hello</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+};
 
-export default App
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    // backgroundColor: '#2c3e50',
+  },
+});
+
+//make this component available to the app
+export default App;
